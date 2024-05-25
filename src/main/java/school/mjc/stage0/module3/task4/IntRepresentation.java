208702e860102cadfe43d99f41bb1ebdb7bc6869
Tog <tog.gavk@gmail.com>
2024-05-25 19:48:51 +0300

    done

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,17 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char ch = 99;
+        System.out.print(ch);
+
+        ch = 104;
+        System.out.print(ch);
+
+
+        ch = 97;
+        System.out.print(ch);
+
+        ch = 114;
+        System.out.print(ch);
     }
 }
